2ae1ea401357cd35b90b1d81bb7b45b933f3ee04
Kynuak <44228098+Kynuak@users.noreply.github.com>
2023-09-27 14:49:53 +0200

    Update navbar.php
    Delete commentary navbar Test

--- navbar.php
+++ navbar.php
@@ -1,27 +1,3 @@
-
-<!--NavBar opé mais pas bonne structure-->
-<!-- <nav id="nav">
-    <div class="navleft">
-        <div class="icons" id="icons"></div>
-        <ul>
-            <li><a href="">Accueil</a></li>
-            <li><a href="">Menu</a></li>
-            <li><a href="">Réservation</a></li>
-        </ul>
-    </div>
-    <div class="centerCircle">
-        <img src="assets/logo.png" alt="" class="logo">
-    </div>
-    <div class="navright">
-        <ul>
-            <li><a href="">Boutique</a></li>
-            <li><a href="">Equipe</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-    </div>
-</nav>     -->
-
-<!--Test structure Navbar -->
 <nav id="nav">
         <div class="icons" id="icons"></div>
         <div class="menu">
