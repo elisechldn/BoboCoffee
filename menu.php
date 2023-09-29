9e1722c125d13ae7bea4e9f44470cf0df716ddef
kykileur <kyliannfernandes@gmail.com>
2023-09-29 11:08:48 +0200

    initialisation branch kyliann

---
+++ menu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bobo Coffee</title>
+  <link rel="stylesheet" href="styles/style.css" />
+  <link rel="stylesheet" href="styles/style_menu.css" />
+</head>
+
+<body>
+  <?php include"navbar.php"; ?>
+  <header>
+  </header>
+  <main>
+    <div class="section">
+    <div> <img src="asset/image/image café.jpg"></div>
+      <div class="menu">
+        <h1> notre menu</h1>
+      <button>
+        <h2>nos boissons fraiche </h2>
+        <img src="asset/image/coca-cola.jpeg">
+        </boutton>
+        <button>
+          <h2>nos boissons chaude </h2>
+          <img src="asset/image/café.jpg">
+        </button>
+        <button>
+          <h2>nos patisseries </h2>
+          <img src="asset/image/croissants.jpg">
+        </button>
+        <button>
+          <h2>nos snacks </h2>
+          <img src="asset/image/hot-dogs.jpg">
+        </button>
+      </div>
+    </div>
+  </main>
+
+</body>
+<script src="script.js"></script>
+
+</html>
