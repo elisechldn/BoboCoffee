9d493b795ee1d5a46b5a089b8d9f2e124c914df0
kykileur <kyliannfernandes@gmail.com>
2023-10-05 17:03:58 +0200

    bonne chance

--- en-menu.php
+++ en-menu.php
@@ -11,26 +11,26 @@
 
 
 <body>
-  <?php include"navbar.php"; ?>
+  <?php include"en-navbar.php"; ?>
   <main>
     <div class="section">
       <img class= "img-back" src="assets/image/image café.jpg">
       <div class="menuPage">
-        <h1> notre menu</h1>
+        <h1> our menu</h1>
       <button>
-        <h2>nos boissons fraiches </h2>
+        <h2>our cold drinks </h2>
         <img src="assets/image/coca-cola.jpeg">
         </boutton>
         <button>
-          <h2>nos boissons chaudes </h2>
+          <h2>our hot drinks </h2>
           <img src="assets/image/café.jpg">
         </button>
         <button>
-          <h2>nos patisseries </h2>
+          <h2>our pastries </h2>
           <img src="assets/image/croissants.jpg">
         </button>
         <button>
-          <h2>nos snacks </h2>
+          <h2>our snacks </h2>
           <img src="assets/image/hot-dogs.jpg">
         </button>
       </div>
