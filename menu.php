d4d20bf97be3397dd58329063fe0abeb595aa3ae
Kynuak <bapt.trouille@gmail.com>
2023-10-06 12:31:11 +0200

    Changement de la page menu et syle menu

--- menu.php
+++ menu.php
@@ -17,27 +17,122 @@
       <img class= "img-back" src="../assets/Menu/Images/image café.jpg">
       <div class="menuPage">
         <h1> notre menu</h1>
-      <button>
-        <h2>nos boissons fraiches </h2>
-        <img src="../assets/Menu/Images/coca-cola.jpeg">
-        </boutton>
-        <button>
-          <h2>nos boissons chaudes </h2>
+      <article class="article-kyliann">   
+        <img src="../assets/Menu/Images/coca-cola.jpeg"> 
+          <div class="card-info"> 
+            <h2>Boissons froides </h2>
+            <ul> 
+              <li> 
+                <h3>Café Tonique au Romarin</h3>
+                <p>Un café glacé au romarin et au citron vert, rehaussé de tonic.</p>
+              </li>
+              <li>
+                <h3>Limonade au Thym</h3>
+                <p>Limonade maison au thym frais et au zeste de citron.</p>
+              </li>
+              <li>
+                <h3>Smoothie Vert Antioxydant</h3>
+                <p>Mélange de kale, épinards, banane, avocat, et lait d'amande.</p>
+              </li>
+              <li>
+                <h3>Café latte glacé à la Lavande</h3>
+                <p>Latte glacé au lait d'amande parumé à la lavande avec une touche de sirop de miel.</p>
+              </li>
+              <li>
+                <h3>Espresso Martini</h3>
+                <p>Espresso, vodka, liqueur de café et sirop d'érable pour une touche de sophistication.</p>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article class="article-kyliann">
           <img src="../assets/Menu/Images/café.jpg">
-        </button>
-        <button>
-          <h2>nos patisseries </h2>
-          <img src="../assets/Menu/Images/croissants.jpg">
-        </button>
-        <button>
-          <h2>nos snacks </h2>
-          <img src="../assets/Menu/Images/hot-dogs.jpg">
-        </button>
+          <div class="card-info"> 
+            <h2>Boissons chaudes</h2>
+            <ul> 
+              <li> 
+                <h3>Café Artisanal</h3>
+                <p>Espresso préparé avec des grains de café biologiques et torréfiés à la boutique.</p>
+              </li>
+              <li>
+                <h3>Café Miel-Cannelle</h3>
+                <p>Espresso mélangé à du miel brut et de la cannelle, garni de crème fouettée.</p>
+              </li>
+              <li>
+                <h3>Café Crème Brûlée</h3>
+                <p>Un latte riche et crémeux avec une touche de caramel brûlé.</p>
+              </li>
+              <li>
+                <h3>Dirty Chai Latte</h3>
+                <p>Un mélange maison de thé chai aux épices chaudes et de lait d'avoine et shot de café.</p>
+              </li>
+              <li>
+                <h3>Matcha Latte</h3>
+                <p>Matcha de qualité supérieure mélangé avec du lait d'amande et garni de pétales de rose comestibles.</p>
+              </li>
+              <li>
+                <h3>Infusion de Gingembre et Curcuma</h3>
+                <p>Une boisson apaisante à base de gingembre frais, de curcuma et de miel.</p>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article class="article-kyliann">
+          <img src="../assets/Menu/Images/muffin.jpg">
+          <div class="card-info"> 
+            <h2>Muffins </h2>
+            <ul> 
+              <li> 
+                <h3>Muffin Chocolat-Coeur café</h3>
+              </li>
+              <li>
+                <h3>Muffin Thé aux Épices</h3>
+              </li>
+              <li>
+                <h3>Muffin Carrot Cake</h3>
+              </li>
+              <li>
+                <h3>Muffin au Matcha et Myrtilles</h3>
+              </li>
+              <li>
+                <h3>Muffin à la Courge et aux Épices d'automne</h3>
+              </li>
+              <li> 
+                <h3>Muffin à la Pomme et au Caramel</h3>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article class="article-kyliann">
+          <img src="../assets/Menu/Images/browni.webp">
+          <div class="card-info"> 
+            <h2>Recettes sucrées</h2>
+            <ul> 
+              <li> 
+                <h3>Brownie au potiron et noix de cajou</h3>
+                </li>
+              <li>
+                <h3>Tarte à la figue et au romarin</h3>
+                </li>
+              <li>
+                <h3>Granola Bowl à la pomme caramélisée, cannelle et noisettes</h3>
+                </li>
+              <li>
+                <h3>Pancakes au potiron, beurre à la cannelle et sirop d'érable</h3>
+              </li>
+              <li>
+                <h3>Rose de pomme et compotée de vanille</h3>
+              </li>
+            </ul>
+          </div>
+      </article>
       </div>
     </div>
   </main>
-
-  <?php include 'footer.php'?>
+  
+  <div class="footerMenu">
+    <?php include 'footer.php'?>
+  </div>
 </body>
 <script src="script.js"></script>
 
--- pages_EN/menu_EN.php
+++ pages_EN/menu_EN.php
@@ -16,29 +16,122 @@
     <div class="section">
       <img class= "img-back" src="../assets/Menu/Images/image café.jpg">
       <div class="menuPage">
-        <h1> our menu</h1>
-      <button>
-        <h2>our cold drinks </h2>
-        <img src="../assets/Menu/Images/coca-cola.jpeg">
-        </boutton>
-        <button>
-          <h2>our hot drinks </h2>
+        <h1> notre menu</h1>
+      <article class="article-kyliann">   
+        <img src="../assets/Menu/Images/coca-cola.jpeg"> 
+          <div class="card-info"> 
+            <h2>Cold drinks</h2>
+            <ul> 
+              <li> 
+                <h3>Rosemary Tonic Coffee</h3>
+                <p>Iced Rosemary and Lime Coffee with Tonic</p>
+              </li>
+              <li>
+                <h3>Thyme Lemonade</h3>
+                <p>Homemade Thyme and Lemon Zest Lemonade</p>
+              </li>
+              <li>
+                <h3>Antioxidant Green Smoothie</h3>
+                <p>Kale, Spinach, Banana, Avocado, and Almond Milk Blend</p>
+              </li>
+              <li>
+                <h3>Iced Lavender Latte</h3>
+                <p>Iced Almond Milk Latte Infused with Lavender and a Hint of Honey Syrup</p>
+              </li>
+              <li>
+                <h3>Espresso Martini</h3>
+                <p>Espresso, vodka, coffee liqueur, and maple syrup for a touch of sophistication</p>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article class="article-kyliann">
           <img src="../assets/Menu/Images/café.jpg">
-        </button>
-        <button>
-          <h2>our pastries </h2>
-          <img src="../assets/Menu/Images/croissants.jpg">
-        </button>
-        <button>
-          <h2>our snacks </h2>
-          <img src="../assets/Menu/Images/hot-dogs.jpg">
-        </button>
+          <div class="card-info"> 
+            <h2>Hot beverages</h2>
+            <ul> 
+              <li> 
+                <h3>Artisanal Coffee</h3>
+                <p>Espresso made with organic, locally roasted coffee beans</p>
+              </li>
+              <li>
+                <h3>Honey Cinnamon Coffee</h3>
+                <p>Espresso blended with raw honey and cinnamon, topped with whipped cream</p>
+              </li>
+              <li>
+                <h3>Café Crème Brûlée</h3>
+                <p>A rich and creamy latte with a hint of burnt caramel</p>
+              </li>
+              <li>
+                <h3>Dirty Chai Latte</h3>
+                <p>House blend of warm spice chai tea and oat milk with a shot of coffee</p>
+              </li>
+              <li>
+                <h3>Matcha Latte</h3>
+                <p>Premium grade matcha blended with almond milk and garnished with edible rose petals</p>
+              </li>
+              <li>
+                <h3>Ginger and Turmeric Infusion</h3>
+                <p>A soothing drink made with fresh ginger, turmeric, and honey</p>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article class="article-kyliann">
+          <img src="../assets/Menu/Images/muffin.jpg">
+          <div class="card-info"> 
+            <h2>Muffins </h2>
+            <ul> 
+              <li> 
+                <h3>Chocolate Coffee Heart Muffin</h3>
+              </li>
+              <li>
+                <h3>Spiced Tea Muffin</h3>
+              </li>
+              <li>
+                <h3>Carrot Cake Muffin</h3>
+              </li>
+              <li>
+                <h3>Matcha Blueberry Muffin</h3>
+              </li>
+              <li>
+                <h3>Autumn Spice Squash Muffin</h3>
+              </li>
+              <li> 
+                <h3>Apple Caramel Muffin</h3>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article class="article-kyliann">
+          <img src="../assets/Menu/Images/browni.webp">
+          <div class="card-info"> 
+            <h2>Sweet Recipes</h2>
+            <ul> 
+              <li> 
+                <h3>Pumpkin and Cashew Brownie</h3>
+                </li>
+              <li>
+                <h3>Fig and Rosemary Tart</h3>
+                </li>
+              <li>
+                <h3>Caramelized Apple, Cinnamon, and Hazelnut Granola Bowl</h3>
+                </li>
+              <li>
+                <h3>Pumpkin Pancakes, Cinnamon Butter and Maple Syrup</h3>
+              </li>
+              <li>
+                <h3>Apple Rose with Vanilla Compote</h3>
+              </li>
+            </ul>
+          </div>
+      </article>
       </div>
     </div>
   </main>
 
   <?php include "footer_EN.php"?>
 </body>
-<script src="../script.js"></script>
+<script src="script.js"></script>
 
 </html>
